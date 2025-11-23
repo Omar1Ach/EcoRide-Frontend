58aead4404db0f6cc92cff1d82aa3e9861e28d48
Omar1Ach <omar.achbani@outlook.com>
2025-11-23 07:35:05 +0100

    Replace flutter_vibrate with vibration package for Android build compatibility

--- lib/core/utils/haptics.dart
+++ lib/core/utils/haptics.dart
@@ -1,34 +1,3 @@
-
-import 'package:flutter_vibrate/flutter_vibrate.dart';
-
-class Haptics {
-  Haptics._();
-
-  static Future<void> light() async {
-    bool canVibrate = await Vibrate.canVibrate;
-    if (canVibrate) {
-      Vibrate.feedback(FeedbackType.light);
-    }
-  }
-
-  static Future<void> medium() async {
-    bool canVibrate = await Vibrate.canVibrate;
-    if (canVibrate) {
-      Vibrate.feedback(FeedbackType.medium);
-    }
-  }
-
-  static Future<void> heavy() async {
-    bool canVibrate = await Vibrate.canVibrate;
-    if (canVibrate) {
-      Vibrate.feedback(FeedbackType.heavy);
-    }
-  }
-
-  static Future<void> success() async {
-    bool canVibrate = await Vibrate.canVibrate;
-    if (canVibrate) {
-      Vibrate.feedback(FeedbackType.success);
     }
   }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import file_selector_macos
 import flutter_secure_storage_macos
 import geolocator_apple
 import mobile_scanner
@@ -13,7 +12,6 @@ import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
