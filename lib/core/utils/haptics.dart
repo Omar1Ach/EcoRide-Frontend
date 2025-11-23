8658bdaa8c1dd75d71e8790909356095d932630e
Omar1Ach <omar.achbani@outlook.com>
2025-11-23 07:38:52 +0100

    Fix dead null-aware expression warnings in haptics

--- lib/core/utils/haptics.dart
+++ lib/core/utils/haptics.dart
@@ -2,37 +2,37 @@ import 'package:vibration/vibration.dart';
 
 class Haptics {
   static Future<void> selection() async {
-    if (await Vibration.hasVibrator() ?? false) {
+    if (await Vibration.hasVibrator() == true) {
       Vibration.vibrate(duration: 10);
     }
   }
 
   static Future<void> light() async {
-    if (await Vibration.hasVibrator() ?? false) {
+    if (await Vibration.hasVibrator() == true) {
       Vibration.vibrate(duration: 20);
     }
   }
 
   static Future<void> medium() async {
-    if (await Vibration.hasVibrator() ?? false) {
+    if (await Vibration.hasVibrator() == true) {
       Vibration.vibrate(duration: 40);
     }
   }
 
   static Future<void> heavy() async {
-    if (await Vibration.hasVibrator() ?? false) {
+    if (await Vibration.hasVibrator() == true) {
       Vibration.vibrate(duration: 60);
     }
   }
 
   static Future<void> success() async {
-    if (await Vibration.hasVibrator() ?? false) {
+    if (await Vibration.hasVibrator() == true) {
       Vibration.vibrate(duration: 50);
     }
   }
 
   static Future<void> error() async {
-    if (await Vibration.hasVibrator() ?? false) {
+    if (await Vibration.hasVibrator() == true) {
       Vibration.vibrate(pattern: [0, 100, 50, 100]);
     }
   }
